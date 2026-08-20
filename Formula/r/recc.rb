a6211dfe3a82eab055df6cbe5b8468798520571d
Patrick Linnane <patrick@linnane.io>
2026-08-20 13:35:07 -0700

    recc: revision bump (protobuf 36.0)

--- Formula/r/recc.rb
+++ Formula/r/recc.rb
@@ -4,6 +4,7 @@ class Recc < Formula
   url "https://gitlab.com/BuildGrid/buildbox/buildbox/-/archive/1.4.18/buildbox-1.4.18.tar.gz"
   sha256 "3749d7a8fa9db38d51f575a45214c4a0fafdde86574523ec6c460c12e7eff6e5"
   license "Apache-2.0"
+  revision 1
   head "https://gitlab.com/BuildGrid/buildbox/buildbox.git", branch: "master"
 
   bottle do
