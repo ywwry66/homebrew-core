8051dc36830539db84011a93ecb4b0dfda5987c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 07:34:51 +0000

    atomgit-cli: add 0.7.2 bottle.

--- Formula/a/atomgit-cli.rb
+++ Formula/a/atomgit-cli.rb
@@ -6,6 +6,15 @@ class AtomgitCli < Formula
   license "MulanPSL-2.0"
   head "https://atomgit.com/hust-open-atom-club/atomgit-cli.git", branch: "main"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "13da2eae0487d94cb19cc868f7b5cc998ee9e5ca23863aae81bb85494d224451"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "13da2eae0487d94cb19cc868f7b5cc998ee9e5ca23863aae81bb85494d224451"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "13da2eae0487d94cb19cc868f7b5cc998ee9e5ca23863aae81bb85494d224451"
+    sha256 cellar: :any_skip_relocation, sonoma:        "8597bf71cefdadb8bece472d08eaf62084a029c6741d8e7f8fcb880094f5edc1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "91f8e97acf9b2475efaa25f28fde5fc2566ea3184b74a58b10fd7034bf4a5f82"
+    sha256 cellar: :any,                 x86_64_linux:  "c712d0fa0b5d3f059eba66708e3dfa44ec08d4da8eb101d929473b95d2df1bef"
+  end
+
   depends_on "go" => :build
 
   def install
