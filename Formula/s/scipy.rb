c990c093e02a7a089ccb0e016e47a662704966fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 23:50:56 +0000

    scipy 1.18.1

--- Formula/s/scipy.rb
+++ Formula/s/scipy.rb
@@ -1,8 +1,8 @@
 class Scipy < Formula
   desc "Software for mathematics, science, and engineering"
   homepage "https://www.scipy.org"
-  url "https://files.pythonhosted.org/packages/a7/25/c2700dfaf6442b4effaa91af24ebce5dc9d31bb4a69706313aae70d72cd0/scipy-1.18.0.tar.gz"
-  sha256 "67b2ad2ad54c72ca6d04975a9b2df8c3638c34ddd5b28738e94fc2b57929d378"
+  url "https://files.pythonhosted.org/packages/7e/74/66de6258867beb2ef08f35f9f2ac017a52cacd5081714d239ff1a442d458/scipy-1.18.1.tar.gz"
+  sha256 "52c4b7422442aba924d03ad4019852b08a92e64ea187b933135687bfe2747307"
   license "BSD-3-Clause"
   compatibility_version 1
   head "https://github.com/scipy/scipy.git", branch: "main"
