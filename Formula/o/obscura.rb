d8f0ebfcf17e46d0634bcdae856ee3648334ddd8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-23 10:52:33 +0000

    obscura: add 0.2.0 bottle.

--- Formula/o/obscura.rb
+++ Formula/o/obscura.rb
@@ -5,6 +5,15 @@ class Obscura < Formula
   sha256 "9eac1a0e40be1cb55ff522a412fa91104f52ebe45431225cd3b532a7d651e3b3"
   license "Apache-2.0"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "479c2d06d5262bf99a509e8a490a27ec7417184048d349ad74f22a78fc3dc904"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f858a062cc31f78dfeb6e6900118745acf04ff0665214879be24f60ac15a4c5c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1a2f373aefa7b6147ef72dd0fde2624b9a19bc875d23bebf6b77ce0e1a876ed2"
+    sha256 cellar: :any_skip_relocation, sonoma:        "260cbf854e5ccafb46d440e4416d193fcba3303264546ef63c2b77b30bf24735"
+    sha256 cellar: :any,                 arm64_linux:   "a961b8fb9be16f221a418a13f3c77e58965f1d35b0ffe65eb844c388cccb42e7"
+    sha256 cellar: :any,                 x86_64_linux:  "8065be39989320380aa4bd7bf77933e88b045e3f44bb03fc5cfeec6dd0d9fa6d"
+  end
+
   depends_on "rust" => :build
 
   def install
