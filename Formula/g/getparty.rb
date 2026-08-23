f383e0be877edceedb7984eb8c90b19cc0828496
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-23 20:01:36 +0000

    getparty: update 1.28.0 bottle.

--- Formula/g/getparty.rb
+++ Formula/g/getparty.rb
@@ -7,12 +7,12 @@ class Getparty < Formula
   head "https://github.com/vbauerster/getparty.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cdf72faf3d9895dfc0876dee26b903a755806dd4b632848cc0824adb86ec060a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cdf72faf3d9895dfc0876dee26b903a755806dd4b632848cc0824adb86ec060a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cdf72faf3d9895dfc0876dee26b903a755806dd4b632848cc0824adb86ec060a"
-    sha256 cellar: :any_skip_relocation, sonoma:        "735316d0f11a0141c84aa4ad81d333109b7bb276450f8a1ed8b66cde6da885bd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1f0b3cc5dcfde96dff8e7b43af4dea7305bb8a0d61d1d20acc983f44c24c1ecc"
-    sha256 cellar: :any,                 x86_64_linux:  "8ddbf85ae5d6b219baef1d1898ce397dd0a5a2246b8b36a1d94b399ae22d40fc"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "90f9832512d1110cfe60c3cb5a41538c5a3546cf5101b102b92a754e9c74af56"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "90f9832512d1110cfe60c3cb5a41538c5a3546cf5101b102b92a754e9c74af56"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "90f9832512d1110cfe60c3cb5a41538c5a3546cf5101b102b92a754e9c74af56"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c07ea509718d64cbfd446f26e9b54de4c7a7656f88e8cbaa47a900ccebd57821"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "98ab8af2101954deb91819b8a837b97305bde5ce96a80e2d2f5bfb01374e75a3"
+    sha256 cellar: :any,                 x86_64_linux:  "77c95a9b5447d96b01e896157007ee05ff02e270df5a1f86b6ab969f32685bc7"
   end
 
   depends_on "go" => :build
