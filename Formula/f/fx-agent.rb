b4d8249bebcd4b4454b06989e0c680aa85376c8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-23 01:04:26 +0000

    fx-agent: add 0.0.5 bottle.

--- Formula/f/fx-agent.rb
+++ Formula/f/fx-agent.rb
@@ -11,6 +11,15 @@ class FxAgent < Formula
     strategy :github_latest
   end
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ace185fc88d19677fd4ef395e0dfa16fda3888ded577aadd3f3cda413cacc811"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b77b09e4e60b1d787d9f1f4c163a4fa445bef413c91629e33f24619cdc730ee5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7d540236be76c76f02a14bbbe3ef4bc591e03947c48c47d6f553a41278c17e57"
+    sha256 cellar: :any_skip_relocation, sonoma:        "2d22d79305050ec901b0d479529263a0f9f871e10314840658776ce83538f9a7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e00ee01560efc7ae044fec18f26d173343660e339238c895a65ea1806388f155"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8b5ebfa08877cc234674976c9c054edecf915a755dcd0cebadcb00137bb42631"
+  end
+
   depends_on "zig" => :build
 
   def install
