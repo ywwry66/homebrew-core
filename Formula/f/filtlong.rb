ecaaa8efe8c635fda6d9df769c7cc9de73cb27bf
Michael Cho <michael@michaelcho.dev>
2026-08-22 20:25:16 -0400

    filtlong: remove incorrect `zlib` dep

--- Formula/f/filtlong.rb
+++ Formula/f/filtlong.rb
@@ -14,7 +14,9 @@ class Filtlong < Formula
     sha256 cellar: :any,                 x86_64_linux:  "b658541b14b15f775f74b3f509dbffece3ac4a4d9cdabe1ca4ac69301fbd4074"
   end
 
-  uses_from_macos "zlib"
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
 
   def install
     system "make"
