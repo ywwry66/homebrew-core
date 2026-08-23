418eb741d5dc65e68b5686a240b9c60c38165cff
Michael Cho <michael@michaelcho.dev>
2026-08-23 19:39:07 -0400

    x265 4.3
    From https://bitbucket.org/multicoreware/x265_git/src/master/
    
    > This repository has been migrated to GitHub. Please use the GitHub
    > repository for all development and issue tracking:
    > https://github.com/Multicorewareinc/x265

--- Formula/x/x265.rb
+++ Formula/x/x265.rb
@@ -1,11 +1,11 @@
 class X265 < Formula
   desc "H.265/HEVC encoder"
-  homepage "https://bitbucket.org/multicoreware/x265_git"
-  url "https://bitbucket.org/multicoreware/x265_git/downloads/x265_4.2.tar.gz"
-  sha256 "40b1ea0453e0309f0eba934e0ddf533f8f6295966679e8894e8f1c1c8d5e1210"
+  homepage "https://github.com/Multicorewareinc/x265"
+  url "https://github.com/Multicorewareinc/x265/releases/download/4.3/x265_4.3.tar.gz"
+  sha256 "83c53e4c8bbb8f1e33ed59e10a7d621d1d7801ca853910c3eb41f038b8ffb121"
   license "GPL-2.0-or-later"
-  compatibility_version 1
-  head "https://bitbucket.org/multicoreware/x265_git.git", branch: "master"
+  compatibility_version 2
+  head "https://github.com/Multicorewareinc/x265.git", branch: "master"
 
   bottle do
     sha256 cellar: :any,                 arm64_tahoe:   "ee3075f8af9807bdb30a890b22c1e948a5930f6001c7123eb57faa87c4d15c85"
