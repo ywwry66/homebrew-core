6b7cefb08d42634fe450d4d7a1b72324d7737c80
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-23 18:05:44 +0000

    utf8cpp 4.2.0

--- Formula/u/utf8cpp.rb
+++ Formula/u/utf8cpp.rb
@@ -1,8 +1,8 @@
 class Utf8cpp < Formula
   desc "UTF-8 with C++ in a Portable Way"
   homepage "https://github.com/nemtrif/utfcpp"
-  url "https://github.com/nemtrif/utfcpp/archive/refs/tags/v4.1.1.tar.gz"
-  sha256 "1ca68016f0abc24172998e39ce0d8f8e2b7a26f7579a0ff85d4e1b9a7aea56f8"
+  url "https://github.com/nemtrif/utfcpp/archive/refs/tags/v4.2.0.tar.gz"
+  sha256 "54a8e96ea835a7359e8e53d03e30e9833d51350cc4615ff53f8449ef19ee46ab"
   license "BSL-1.0"
   version_scheme 1
   compatibility_version 1
@@ -14,9 +14,6 @@ class Utf8cpp < Formula
   depends_on "cmake" => [:build, :test]
 
   def install
-    # Temporary fix, remove in next release
-    inreplace "CMakeLists.txt", "VERSION 4.1.0", "VERSION #{version}"
-
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
