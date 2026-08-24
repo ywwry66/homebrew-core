fa7df3ea3d0781a564d6202f17877844ae814709
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 18:18:21 +0000

    quill 12.2.2

--- Formula/q/quill.rb
+++ Formula/q/quill.rb
@@ -1,8 +1,8 @@
 class Quill < Formula
   desc "C++17 Asynchronous Low Latency Logging Library"
   homepage "https://github.com/odygrd/quill"
-  url "https://github.com/odygrd/quill/archive/refs/tags/v12.2.1.tar.gz"
-  sha256 "dc44e31d0eddfdcf4b92c90793add306573e784bedd9dced81d4ba53406da2f9"
+  url "https://github.com/odygrd/quill/archive/refs/tags/v12.2.2.tar.gz"
+  sha256 "0daa25874ad15fa17a5cd8964ddb184ee120bed6090575c686217be152a5d7cd"
   license "MIT"
   head "https://github.com/odygrd/quill.git", branch: "master"
 
