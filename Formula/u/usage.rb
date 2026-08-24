2d3bc213b84dbad107799a60bfb23134e911c9c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 21:09:00 +0000

    usage 6.4.0

--- Formula/u/usage.rb
+++ Formula/u/usage.rb
@@ -1,8 +1,8 @@
 class Usage < Formula
   desc "Tool for working with usage-spec CLIs"
   homepage "https://usage.jdx.dev/"
-  url "https://github.com/jdx/usage/archive/refs/tags/v6.3.0.tar.gz"
-  sha256 "5f587fdaf34fe52b6613a91bb8212da2b99cd8f7dbeaac4bc1628a168bd20bd9"
+  url "https://github.com/jdx/usage/archive/refs/tags/v6.4.0.tar.gz"
+  sha256 "7a497928257809b325e7da1d7b1497931385999dec1f7bd1c695f9218783515d"
   license "MIT"
   compatibility_version 1
   head "https://github.com/jdx/usage.git", branch: "main"
