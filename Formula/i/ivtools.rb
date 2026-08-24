f5406720f1e7c608d58937b51d8549a6af138582
botantony <antonsm21@gmail.com>
2026-08-24 11:02:06 +0200

    ivtools: revision bump (ace 8.0.7)
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Formula/i/ivtools.rb
+++ Formula/i/ivtools.rb
@@ -4,7 +4,7 @@ class Ivtools < Formula
   url "https://github.com/vectaport/ivtools/archive/refs/tags/ivtools-2.1.1.tar.gz"
   sha256 "4fa680b9d0fd2af610e9012258e150249d9a8abbc3b1f65eb17d01c959142cfc"
   license "MIT"
-  revision 5
+  revision 6
 
   livecheck do
     url :stable
