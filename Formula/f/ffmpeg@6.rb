e6fc079edeb9cf179b143e459f5778853691b9ec
Michael Cho <20700669+cho-m@users.noreply.github.com>
2026-08-24 12:44:50 +0000

    ffmpeg@6: update 6.1.6_3 bottle.

--- Formula/f/ffmpeg@6.rb
+++ Formula/f/ffmpeg@6.rb
@@ -16,12 +16,12 @@ class FfmpegAT6 < Formula
   end
 
   bottle do
-    sha256 arm64_tahoe:   "e289fb5d26bbdd9bf2b949db56f6251fa28880b9be4798b9186734888531ef3f"
-    sha256 arm64_sequoia: "8e183ebc3788a9fc2a277440cf76d7001ab3ea981180eeb899bcee5a49e2a2e8"
-    sha256 arm64_sonoma:  "2ceaad7dbb7a4a183b64941b43cc29f34f00ac57e2db2f14b280b1120c68cca1"
-    sha256 sonoma:        "bf0e605cd2cc9580de66cf7485b45e48492835a8630879d96431c2b34df77538"
-    sha256 arm64_linux:   "58737044840e78bf254e3e44433cf834f7411b6579bd31e34e3569b9d3889f8d"
-    sha256 x86_64_linux:  "c8d15948fe1fcf2f67ade0eb3ccca23f7e4a53d41d16b763a9999f3273f7ae28"
+    sha256 arm64_tahoe:   "a4d5c60ea0a8304b4152dd3296cf0caa5a101607df6e6fd1153933ee1af75af3"
+    sha256 arm64_sequoia: "2408a68098ddb6cc3db8a45afa96aa6503aa391f6b09fb7d5cc8074be1aa55b9"
+    sha256 arm64_sonoma:  "8ab1d0b2e43794725a5393f35c448889288d19ca87c0be81c9b618667cfae27d"
+    sha256 sonoma:        "7da18c2b2806433b24fd05a73e4d22dab4793451b55340d6441d1d9bb3e7bf57"
+    sha256 arm64_linux:   "81eaf92960b5149036ba3cfee2074d3abd3747f083c0a9c87c3724b90ab04c11"
+    sha256 x86_64_linux:  "b007db0a668362b32cd8d87c61d7a23b29571539b7bd6f97e6ab2c936e21d2ae"
   end
 
   keg_only :versioned_formula
