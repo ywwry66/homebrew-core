83164a2520657acd699aec49e024ca418c79c331
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 23:38:57 +0000

    limine 12.6.1

--- Formula/l/limine.rb
+++ Formula/l/limine.rb
@@ -1,8 +1,8 @@
 class Limine < Formula
-  desc "Modern, advanced, portable, multiprotocol bootloader and boot manager"
+  desc "Modern, secure, portable, multiprotocol bootloader and boot manager"
   homepage "https://github.com/Limine-Bootloader/Limine"
-  url "https://github.com/Limine-Bootloader/Limine/releases/download/v12.6.0/limine-12.6.0.tar.gz"
-  sha256 "3177b8a64297667a379feb3af6405b13c536409ce46d712ea1233e9ed1f87b9a"
+  url "https://github.com/Limine-Bootloader/Limine/releases/download/v12.6.1/limine-12.6.1.tar.gz"
+  sha256 "76de3768cdd106ff54cdcbaa25c563971ee41ea17b11a9b0ffd5573d2b025c00"
   license "BSD-2-Clause"
 
   livecheck do
@@ -30,20 +30,10 @@ class Limine < Formula
   depends_on "mtools" => :build
   depends_on "nasm" => :build
 
-  on_macos do
-    # Make < 3.82 picks matching implicit pattern rules in definition order instead of shortest-stem order
-    # (https://lists.gnu.org/archive/html/info-gnu/2010-07/msg00023.html)
-    #
-    # Upstream is aware of this issue, try moving back to system `make` on next release.
-    depends_on "make" => :build
-  end
-
   def install
     # Homebrew LLVM is not in path by default. Get the path to it, and override the
     # build system's defaults for the target tools.
     llvm_bins = formula_opt_bin("llvm")
-
-    ENV.prepend_path "PATH", Formula["make"].libexec/"gnubin" if OS.mac?
 
     system "./configure", *std_configure_args, "--enable-all",
            "TOOLCHAIN_FOR_TARGET=#{llvm_bins}/llvm-",
