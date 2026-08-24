1f9813f68de0bbcf93721380c3d07055205c22c9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 14:34:37 +0000

    httptap 0.6.0

--- Formula/h/httptap.rb
+++ Formula/h/httptap.rb
@@ -3,8 +3,8 @@ class Httptap < Formula
 
   desc "HTTP request visualizer with phase-by-phase timing breakdown"
   homepage "https://httptap.dev"
-  url "https://files.pythonhosted.org/packages/40/48/fc97a49ec7f1065de53732a0ab9d60aecba26dfc12923dd6310be1c52b0e/httptap-0.5.4.tar.gz"
-  sha256 "dd08cd2b7a3262f2f00c34dab439555903912c83738928e00e0b58d64b3257e8"
+  url "https://files.pythonhosted.org/packages/f7/4a/fcdd846abde9f55268026eb7432cd762fb593cdd271431b3941f2b716583/httptap-0.6.0.tar.gz"
+  sha256 "608d1980a0d8379cbcc945bc39125ec8831bb3a77b879fa4ddf7dea452e48843"
   license "Apache-2.0"
 
   bottle do
