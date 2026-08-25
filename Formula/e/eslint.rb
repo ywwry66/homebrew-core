bc605f3c1ecb6fd0c99a47efa3c0e84fa75db705
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-25 20:16:29 +0000

    eslint 10.9.1

--- Formula/e/eslint.rb
+++ Formula/e/eslint.rb
@@ -1,8 +1,8 @@
 class Eslint < Formula
   desc "AST-based pattern checker for JavaScript"
   homepage "https://eslint.org"
-  url "https://registry.npmjs.org/eslint/-/eslint-10.9.0.tgz"
-  sha256 "7c0636e6b1777c04674783f876ed23ec41c30a57466f1dc47a4a2943fcca5144"
+  url "https://registry.npmjs.org/eslint/-/eslint-10.9.1.tgz"
+  sha256 "e7aebee5effb01785c583b3e0f2fb28018c88bc9eb52dc6896a2901c496af5b8"
   license "MIT"
   head "https://github.com/eslint/eslint.git", branch: "main"
 
