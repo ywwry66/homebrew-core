4a4147961f7f1f1db693108fa5c349c45e984ebb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-25 08:58:00 +0000

    mmctl 11.10.1

--- Formula/m/mmctl.rb
+++ Formula/m/mmctl.rb
@@ -1,8 +1,8 @@
 class Mmctl < Formula
   desc "Remote CLI tool for Mattermost server"
   homepage "https://mattermost.com"
-  url "https://github.com/mattermost/mattermost/archive/refs/tags/v11.10.0.tar.gz"
-  sha256 "1adc659e1cddd03e33398981c23ae5c88dbee11d6c01d66c58b5c715473dd0b7"
+  url "https://github.com/mattermost/mattermost/archive/refs/tags/v11.10.1.tar.gz"
+  sha256 "b749516d50ce24c55aea7d4eb25d774fcb91a65ef313e489f469aeca00a20d6c"
   license all_of: ["AGPL-3.0-only", "Apache-2.0"]
   head "https://github.com/mattermost/mattermost.git", branch: "master"
 
