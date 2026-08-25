cad0b1c8ef6ac4c05dec544108f905071b5ddc7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-25 19:03:11 +0000

    tronbyt-server 2.3.5

--- Formula/t/tronbyt-server.rb
+++ Formula/t/tronbyt-server.rb
@@ -1,8 +1,8 @@
 class TronbytServer < Formula
   desc "Manage your apps on your Tronbyt (flashed Tidbyt) completely locally"
   homepage "https://github.com/tronbyt/server"
-  url "https://github.com/tronbyt/server/archive/refs/tags/v2.3.4.tar.gz"
-  sha256 "2ce4b42d8fa6b6eb57115c2d35f3df13ffd06d36386f820c90295bb363eb6dce"
+  url "https://github.com/tronbyt/server/archive/refs/tags/v2.3.5.tar.gz"
+  sha256 "e5b7c05c882613428010428c4e4455f7af229f510aa8efbc7817d760b68abdf0"
   license "Apache-2.0"
   head "https://github.com/tronbyt/server.git", branch: "main"
 
