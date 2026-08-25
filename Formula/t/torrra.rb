3b3f1271dbc75ddfa3fe43272083c2e658dea86a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-25 22:19:23 +0000

    torrra: update 2.3.0 bottle.

--- Formula/t/torrra.rb
+++ Formula/t/torrra.rb
@@ -14,7 +14,7 @@ class Torrra < Formula
   no_autobump! because: "`update-python-resources` cannot determine dependencies"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "e9d432227bd5830572140e521630b7f07c396ed013a1667a0ae330e7b45d7d68"
+    sha256 cellar: :any_skip_relocation, all: "3e3d1f17dbcea2eaeebeb92b5f252f2674cca535a1f33ad7ac1ae2af527e6468"
   end
 
   depends_on "certifi" => :no_linkage
