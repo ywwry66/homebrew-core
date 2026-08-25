03f6cbce738544c68d65a43df2a6664a62e99df5
Sean Molenaar <smillerdev@me.com>
2026-08-25 15:40:28 +0200

    pythia: add 8.317 bottle.

--- Formula/p/pythia.rb
+++ Formula/p/pythia.rb
@@ -16,6 +16,15 @@ class Pythia < Formula
 
   no_autobump! because: :incompatible_version_format
 
+  bottle do
+    sha256  arm64_tahoe:   "1254d29f44c58c38080f054bd2341fc93656a5c109e1fca5ad5d88c66eb3f275"
+    sha256  arm64_sequoia: "aaa62ca8c835af290e8fc06b6536a03b9961b0a23cced3ef7c9789748ef55b19"
+    sha256  arm64_sonoma:  "fbc6e3f328264e825420de00f817fde3f9afe7ff87dd3a05f229e1147b5fa5c3"
+    sha256  sonoma:        "24ebb6e7f4597424eecaeec1803205ac5b000e9fa5d7caf449e463b927e2d651"
+    sha256  arm64_linux:   "a11272e81563f679c978e471ce9da285d148f19edc83ff5afbe55e0b0c7bd1b5"
+    sha256  x86_64_linux:  "2a3b4b0846b766c5428bae60d3e46d62ef7c171f205ba1e44f22d75252f0ad2f"
+  end
+
   depends_on "rsync" => :build
 
   def install
