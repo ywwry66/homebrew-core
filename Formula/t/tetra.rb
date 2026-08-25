a81c5e399284b1c92d65aa4654fc7e3dec6809c4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-25 13:22:23 +0000

    tetra: update 1.7.1 bottle.

--- Formula/t/tetra.rb
+++ Formula/t/tetra.rb
@@ -11,12 +11,12 @@ class Tetra < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "29aa4d37b1ff8bdd54b6a4039dd292db75b79695565d2cc4ecccf70f89074b11"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "84d872433dac34a4357359cc8d49a15e2327846cbbecc247bac98d7e11a7c6d4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "de806a15af6d8b30163988588f8dffaa44f0f6f9a2714c7a29fe858b822419f6"
-    sha256 cellar: :any_skip_relocation, sonoma:        "924b2013ada6d0742ae86b7d4b608d6d3113812579b5a78a02fe5372ba5a0dc5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "30d831fbea91449d536d6b98576640831d9fea9d63893e23028b070da1bd09ac"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d9e1a3632043b8a745a4d9ab97615a5454b45955fb98f043427d1737600ac43f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "19de6f1783d1d9b2e33860c172d033a5abd4329969ad880633816687e08f1403"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8362b1a39aed776aceac2caeedd170eed45931c4ee281e5c37499357afece3a0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d3eaa89db45b7578beeb8045fe2368089f945f8bce6878c7d0f2c300df8f138"
+    sha256 cellar: :any_skip_relocation, sonoma:        "6ed24fa9479f2265d4b1f735193b67d4ceb378621b48ad776a7e121fe8a6f0db"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "40bdd8a674cf61b2775599e494f8d1a04f7ba8b5daf4649e764940868599a971"
+    sha256 cellar: :any,                 x86_64_linux:  "4b293c3e5579cf7eb19f4b44b5c34e60d894bc6d98a142d26e919ddaae65be5c"
   end
 
   depends_on "go" => :build
