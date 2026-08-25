c34a0de3ef3594d7ea6abfe4d7cd4aff18aadf29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-25 20:10:35 +0000

    crane 0.22.0

--- Formula/c/crane.rb
+++ Formula/c/crane.rb
@@ -1,8 +1,8 @@
 class Crane < Formula
   desc "Tool for interacting with remote images and registries"
   homepage "https://github.com/google/go-containerregistry"
-  url "https://github.com/google/go-containerregistry/archive/refs/tags/v0.21.9.tar.gz"
-  sha256 "6d8bce869afcc485b518cc0d59ea0ffe1090026db965806bc3be8793182528cc"
+  url "https://github.com/google/go-containerregistry/archive/refs/tags/v0.22.0.tar.gz"
+  sha256 "52f10d0154053b7bdc18bc451a6d3076f4442a664236cb91e8a1258bd210af09"
   license "Apache-2.0"
   head "https://github.com/google/go-containerregistry.git", branch: "main"
 
