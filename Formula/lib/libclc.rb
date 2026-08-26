2211e696bbf28e71cf9cd1b6e44fae258b7ce359
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-26 11:44:15 +0000

    libclc 23.1.0

--- Formula/lib/libclc.rb
+++ Formula/lib/libclc.rb
@@ -1,8 +1,8 @@
 class Libclc < Formula
   desc "Implementation of the library requirements of the OpenCL C programming language"
   homepage "https://libclc.llvm.org/"
-  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-22.1.8/llvm-project-22.1.8.src.tar.xz"
-  sha256 "922f1817a0df7b1489272d18134ee0087a8b068828f87ac63b9861b1a9965888"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-23.1.0/llvm-project-23.1.0.src.tar.xz"
+  sha256 "ab1f0e3ec52448c33e8782eaf0422504b87c7b016b22514653ee0d8fcee479ff"
   license "Apache-2.0" => { with: "LLVM-exception" }
   compatibility_version 1
 
@@ -25,34 +25,50 @@ class Libclc < Formula
   depends_on "spirv-llvm-translator" => :build
 
   def install
-    llvm_spirv = formula_opt_bin("spirv-llvm-translator")/"llvm-spirv"
-    system "cmake", "-S", "libclc", "-B", "build",
-                    "-DLLVM_SPIRV=#{llvm_spirv}",
-                    *std_cmake_args
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+    targets = %w[
+      amdgcn-amd-amdhsa-llvm
+      nvptx64-nvidia-cuda
+      spirv32-unknown-unknown
+      spirv64-unknown-unknown
+      spirv32-unknown-vulkan
+      spirv64-unknown-vulkan
+    ]
 
-    inreplace share/"pkgconfig/libclc.pc", prefix, opt_prefix
+    # Targets are cross-compiled and incompatible with shim-injected flags like `-march`/`-mbranch-protection`
+    args = ["-DCMAKE_CLC_COMPILER=#{formula_opt_bin("llvm")}/clang"]
+
+    targets.each do |target|
+      builddir = "build-#{target}"
+      system "cmake", "-S", "libclc", "-B", builddir, "-DLLVM_DEFAULT_TARGET_TRIPLE=#{target}", *args, *std_cmake_args
+      system "cmake", "--build", builddir
+      system "cmake", "--install", builddir
+    end
   end
 
   test do
+    # https://github.com/llvm/llvm-project/blob/main/libclc/test/integer/add_sat.cl
     (testpath/"add_sat.cl").write <<~C
-      __kernel void foo(__global char *a, __global char *b, __global char *c) {
-        *a = add_sat(*b, *c);
+      char test_char(char x, char y) {
+        return add_sat(x, y);
       }
     C
 
+    target = "amdgcn-amd-amdhsa-llvm"
     clang_args = %W[
-      -target nvptx64--nvidiacl
-      -c -emit-llvm
-      -Xclang -mlink-bitcode-file
-      -Xclang #{share}/clc/nvptx64--nvidiacl.bc
+      --target=#{target}
+      -mcpu=gfx900
+      --libclc-lib=:#{share}/clc/#{target}/libclc.bc
+      -cl-std=CL3.0
+      -O2
+      -fno-discard-value-names
+      -emit-llvm
+      -S
     ]
     llvm_bin = formula_opt_bin("llvm")
 
     system llvm_bin/"clang", *clang_args, "./add_sat.cl"
-    ir = shell_output("#{llvm_bin}/llvm-dis ./add_sat.bc -o -")
-    assert_match('target triple = "nvptx64-unknown-nvidiacl"', ir)
-    assert_match(/define .* @foo\(/, ir)
+    ir = File.read("add_sat.ll")
+    assert_match("target triple = \"#{target}\"", ir)
+    assert_match(/define .* @test_char\(/, ir)
   end
 end
