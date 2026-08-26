7e1da99d4e64748ff0081e2a843acee3ac15d8f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-26 15:09:12 +0000

    rollup 4.63.0

--- Formula/r/rollup.rb
+++ Formula/r/rollup.rb
@@ -1,8 +1,8 @@
 class Rollup < Formula
   desc "Next-generation ES module bundler"
   homepage "https://rollupjs.org/"
-  url "https://registry.npmjs.org/rollup/-/rollup-4.62.5.tgz"
-  sha256 "4639a03c2f5dc13c66485cb2ef1e4b53ee6067c255159ee67b93b534e50410af"
+  url "https://registry.npmjs.org/rollup/-/rollup-4.63.0.tgz"
+  sha256 "301dfb07a8f295171ef065309ca5b481d06a2f6eb7da5500f6aca731fd446c04"
   license all_of: ["ISC", "MIT"]
 
   bottle do
