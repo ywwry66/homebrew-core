79006bd959da961e189b24e516960e71ec56a21d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-27 17:40:08 +0000

    b4n 0.5.7

--- Formula/b/b4n.rb
+++ Formula/b/b4n.rb
@@ -1,8 +1,8 @@
 class B4n < Formula
   desc "Terminal user interface (TUI) for Kubernetes API written in Rust"
   homepage "https://github.com/fioletoven/b4n"
-  url "https://github.com/fioletoven/b4n/archive/refs/tags/v0.5.6.tar.gz"
-  sha256 "c016fa33229c5be961d0dfe9ab23aede7404c184f59135f85f11af685c43d973"
+  url "https://github.com/fioletoven/b4n/archive/refs/tags/v0.5.7.tar.gz"
+  sha256 "4cd40d6bc08e69a880c924c290af90ad59b48211797dc8dc9daa586c362a2e90"
   license "MIT"
   head "https://github.com/fioletoven/b4n.git", branch: "main"
 
