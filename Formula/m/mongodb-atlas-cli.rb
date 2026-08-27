1825fbf3d38dfb403b1e26b593dc6c57823e4341
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-27 18:53:57 +0000

    mongodb-atlas-cli 1.58.2

--- Formula/m/mongodb-atlas-cli.rb
+++ Formula/m/mongodb-atlas-cli.rb
@@ -1,8 +1,8 @@
 class MongodbAtlasCli < Formula
   desc "Atlas CLI enables you to manage your MongoDB Atlas"
   homepage "https://www.mongodb.com/docs/atlas/cli/stable/"
-  url "https://github.com/mongodb/mongodb-atlas-cli/archive/refs/tags/atlascli/v1.58.1.tar.gz"
-  sha256 "ea5279bca8e33ad9032d9293a8f58da3628ce68e29ddb728e8b3144419326287"
+  url "https://github.com/mongodb/mongodb-atlas-cli/archive/refs/tags/atlascli/v1.58.2.tar.gz"
+  sha256 "ad79c3f12763b5fe359c343e7d949484b43d2fccf83fcd15b823c6d82ae51cf6"
   license "Apache-2.0"
   head "https://github.com/mongodb/mongodb-atlas-cli.git", branch: "master"
 
