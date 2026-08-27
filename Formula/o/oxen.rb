ac9ef7f9a65100b6d455dd6aaa148a9ad2d6802a
Greg Schoeninger <greg.schoeninger@gmail.com>
2026-08-27 15:52:39 +0000

    oxen 0.55.0

--- Formula/o/oxen.rb
+++ Formula/o/oxen.rb
@@ -1,8 +1,8 @@
 class Oxen < Formula
   desc "Data VCS for structured and unstructured machine learning datasets"
   homepage "https://www.oxen.ai/"
-  url "https://github.com/Oxen-AI/Oxen/archive/refs/tags/v0.53.4.tar.gz"
-  sha256 "81ef2710f41c61fb8e417df4972f662e9defbc2524c0e68af2079c7743c3fef0"
+  url "https://github.com/Oxen-AI/Oxen/archive/refs/tags/v0.55.0.tar.gz"
+  sha256 "342b139f0b76d864b867d4ed5bd1d42d2cf6dfeb8c494ff8fa333df84b67ee20"
   license "Apache-2.0"
   head "https://github.com/Oxen-AI/Oxen.git", branch: "main"
 
