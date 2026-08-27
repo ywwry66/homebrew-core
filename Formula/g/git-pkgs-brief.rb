340852d55ddfa36104accbb6cdc3e2e5d147a781
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-27 17:59:26 +0000

    git-pkgs-brief 0.12.1

--- Formula/g/git-pkgs-brief.rb
+++ Formula/g/git-pkgs-brief.rb
@@ -1,8 +1,8 @@
 class GitPkgsBrief < Formula
   desc "Tool that detects and reports a project's toolchain, configuration, and more"
   homepage "https://github.com/git-pkgs/brief"
-  url "https://github.com/git-pkgs/brief/archive/refs/tags/v0.12.0.tar.gz"
-  sha256 "c0f54b886fec1c6bdefe46d6a4f1fb9f82310853fff8ac6042ef5f07eca35183"
+  url "https://github.com/git-pkgs/brief/archive/refs/tags/v0.12.1.tar.gz"
+  sha256 "6a425c3855981475711b6bf8cab09f2dce375d40cc18233c240311c0a74dd825"
   license "MIT"
   head "https://github.com/git-pkgs/brief.git", branch: "main"
 
