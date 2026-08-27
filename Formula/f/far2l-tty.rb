32c5c114abb7ffbeff56a8efb7d8501ba00bb0ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-27 23:20:14 +0000

    far2l-tty 2.9.0

--- Formula/f/far2l-tty.rb
+++ Formula/f/far2l-tty.rb
@@ -1,10 +1,9 @@
 class Far2lTty < Formula
   desc "Unix TTY port of FAR Manager v2 (with NetRocks support)"
   homepage "https://github.com/elfmz/far2l"
-  url "https://github.com/elfmz/far2l/archive/refs/tags/v_2.8.0.tar.gz"
-  sha256 "b0fddad2e3985f245f9e691e23b90fb97f7d29d9a0b131fe686aa3cbb2e4ea01"
+  url "https://github.com/elfmz/far2l/archive/refs/tags/v_2.9.0.tar.gz"
+  sha256 "69a5218fcfd072a2d4b99ecac8363a67d85f2fd67b65243f8ea7b239bb134ed0"
   license "GPL-2.0-only"
-  revision 1
 
   livecheck do
     url :stable
