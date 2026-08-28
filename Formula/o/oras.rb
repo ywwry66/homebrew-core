654aa8a7002d92fb9e5c1fdcd662953a8ac8f3e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-28 01:57:44 +0000

    oras: update 1.3.4 bottle.

--- Formula/o/oras.rb
+++ Formula/o/oras.rb
@@ -12,12 +12,11 @@ class Oras < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "de03e01f23509ff7ba40d2bc0b0dd01040df19e25a14ba2fa87600903f1a5b59"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "de03e01f23509ff7ba40d2bc0b0dd01040df19e25a14ba2fa87600903f1a5b59"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "de03e01f23509ff7ba40d2bc0b0dd01040df19e25a14ba2fa87600903f1a5b59"
-    sha256 cellar: :any_skip_relocation, sonoma:        "7a80978e2364878e9aa76ca4bbdc9d813f6c077f47b600c666a004a34f9e6772"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9a24178505ef17aa22bc6815c5c41a024447e43dfe230ec64d2c3c79c647cdfe"
-    sha256 cellar: :any,                 x86_64_linux:  "7f54ab4408b7820c8bdf2ad60de53cb95626426dee50feb2c88badc93337d05c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1749e36c351467261a270c29a89f0e87059829f56899f0a6dcdd8a3938fb833e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1749e36c351467261a270c29a89f0e87059829f56899f0a6dcdd8a3938fb833e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1749e36c351467261a270c29a89f0e87059829f56899f0a6dcdd8a3938fb833e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "292ce96bfa33eca540ba1ebf6297ffb180c131f0908482edf3fe45672759edea"
+    sha256 cellar: :any,                 x86_64_linux:  "ce9c2d062d70f4cd4a627fa2bc566e5c4df150298b3ac69d95fe6e3841984229"
   end
 
   depends_on "go" => :build
