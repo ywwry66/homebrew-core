7e5e21884fd64350e7b5109547a337b69154430f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-28 04:27:34 +0000

    asn 0.81.1

--- Formula/a/asn.rb
+++ Formula/a/asn.rb
@@ -1,8 +1,8 @@
 class Asn < Formula
   desc "Organization lookup and server tool (ASN / IPv4 / IPv6 / Prefix / AS Path)"
   homepage "https://github.com/nitefood/asn"
-  url "https://github.com/nitefood/asn/archive/refs/tags/v0.81.0.tar.gz"
-  sha256 "b17aafcff2e4763002c352ccbaa85b9d3ac3c45770e7062e82e9ad18fa1798df"
+  url "https://github.com/nitefood/asn/archive/refs/tags/v0.81.1.tar.gz"
+  sha256 "43680a3b77cae81ff86210fdd42bc1c9d9e17dbec58a7c6772eb8b7b0188de25"
   license "MIT"
   head "https://github.com/nitefood/asn.git", branch: "master"
 
