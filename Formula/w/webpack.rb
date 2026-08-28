75b46ba6b001b372cb19293f301fb29a769c8cdf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-28 13:35:04 +0000

    webpack: update 5.110.0 bottle.

--- Formula/w/webpack.rb
+++ Formula/w/webpack.rb
@@ -9,12 +9,7 @@ class Webpack < Formula
   head "https://github.com/webpack/webpack.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b56aab962de498a33f9d966babe9edb976764d96c660926e2f978936ad7f9168"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b56aab962de498a33f9d966babe9edb976764d96c660926e2f978936ad7f9168"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b56aab962de498a33f9d966babe9edb976764d96c660926e2f978936ad7f9168"
-    sha256 cellar: :any_skip_relocation, sonoma:        "61d5fae456accab215aa861b1482aff0ab932e8639db983dce2caa7d958001af"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b56aab962de498a33f9d966babe9edb976764d96c660926e2f978936ad7f9168"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b56aab962de498a33f9d966babe9edb976764d96c660926e2f978936ad7f9168"
+    sha256 cellar: :any_skip_relocation, all: "0b175591939e1615905c87e64b5675350805590e443c928124fab7cf291e00a1"
   end
 
   depends_on "node"
