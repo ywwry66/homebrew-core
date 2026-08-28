d674997decc5f1bb28c3e2e747c72bd3a0211dbb
botantony <antonsm21@gmail.com>
2026-08-28 22:29:49 +0200

    maxima: revision bump (sbcl 2.6.8)

--- Formula/m/maxima.rb
+++ Formula/m/maxima.rb
@@ -4,6 +4,7 @@ class Maxima < Formula
   url "https://downloads.sourceforge.net/project/maxima/Maxima-source/5.50.0-source/maxima-5.50.0.tar.gz"
   sha256 "0bc4b5e11fe153ef20b24a3a816b668ece5378cc738fa24ca426b62fd6d8fc37"
   license "GPL-2.0-only"
+  revision 1
 
   livecheck do
     url :stable
