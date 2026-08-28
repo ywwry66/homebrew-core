f14fb631676656784d66411198d351e0abd465da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-28 09:59:19 +0000

    pixivbiu: add 3.0.1 bottle.

--- Formula/p/pixivbiu.rb
+++ Formula/p/pixivbiu.rb
@@ -5,6 +5,15 @@ class Pixivbiu < Formula
   sha256 "d657b9da0f2c845b75b2418fad30f622e895d12c386557797073b4c3e000a348"
   license "MIT"
 
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "403308d0003d9790f73e4583c1aa61337ed7ef32f16a25cc630e10f677dee63b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d11e288bdd8c924384be9d4a755e078fb14b13a00c0f12aeda1dd61c32b412ba"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c218bc328b1f6796db3b3bda670f0d821c67a62c783a39f7c8c8564a86498bdf"
+    sha256 cellar: :any_skip_relocation, sonoma:        "c91acf8beee46b53c2f708f8dcf32209c89219cfd0238ea9e2ef996a213b1a01"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9b3424df7f7933517faedfb79be37828ef6431518f049ccd98020601a8f0aefd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c1791b748f2fe74b5cb369b1a2fc05b586f2ff26004a817698dd04da9d6d30f1"
+  end
+
   depends_on "bun" => :build
   depends_on "go" => :build
 
