74f2dc557d1eee75db20197e62339825d3a884d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-28 10:18:05 +0000

    mapnik: update 4.3.1 bottle.

--- Formula/m/mapnik.rb
+++ Formula/m/mapnik.rb
@@ -12,12 +12,11 @@ class Mapnik < Formula
   end
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "2cf57467d0bcd2e73d4edef4db84b626bdd934e3e455d9a70fbdb7cd18b3569a"
-    sha256 cellar: :any, arm64_sequoia: "79ab2ac708dd1be0cf8a0b21cdfcc10d4f8483a050f95671ffae4de58ad8619f"
-    sha256 cellar: :any, arm64_sonoma:  "4960f69f6a7efcccb4cdd62bf692bb8ca7823ec55905cdfe204603b9710e872c"
-    sha256 cellar: :any, sonoma:        "dbf5eba54315fa3eafdea977bfe4e99a35cdd62a8fb26744e02ee3242b414b29"
-    sha256 cellar: :any, arm64_linux:   "b186311caf300288f1ce5751458c9b5ed8bacb273938313b77f2af8fd7dfe98d"
-    sha256 cellar: :any, x86_64_linux:  "b7cf806b1df3616695de3bb037cb7492cdcf1ced628befb068d2c259889fcc78"
+    sha256 cellar: :any, arm64_tahoe:   "99db6651491ca3a306f61d5cbf6bca857f5031e1c13e982cdc72b7d9e5c76c80"
+    sha256 cellar: :any, arm64_sequoia: "64bbeca5e052b964c53ec208f4b5119c78d2270ded7f13d5682822baa0861cbd"
+    sha256 cellar: :any, arm64_sonoma:  "f27b4e8159b53d0f6ce40cb21e1ffc53adbb423b10c1d236614243af02cac79c"
+    sha256 cellar: :any, arm64_linux:   "f2c15b9d4d17f09a399172b6461b8e1d7b12a83812d84c71f237937429254c43"
+    sha256 cellar: :any, x86_64_linux:  "79ec3d1289b7955084669787f0d1a5cac52f2eb932d0d796af5002bd97ded36c"
   end
 
   depends_on "cmake" => :build
