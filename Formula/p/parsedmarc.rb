8c0244f82e446b321ab2fff337cd57b17236d158
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-29 21:41:26 +0000

    parsedmarc: update 10.5.0 bottle.

--- Formula/p/parsedmarc.rb
+++ Formula/p/parsedmarc.rb
@@ -9,12 +9,11 @@ class Parsedmarc < Formula
   head "https://github.com/domainaware/parsedmarc.git", branch: "master"
 
   bottle do
-    sha256 cellar: :any, arm64_tahoe:   "e5a82ee9f0d47081b683691bec6a5a44b80118e3ef2320f1ccad43158d4cdd9e"
-    sha256 cellar: :any, arm64_sequoia: "798f6d8d47212290d9e5f6e82ff472216220582beadf815ee01b41e55429e443"
-    sha256 cellar: :any, arm64_sonoma:  "76cba78893f6cc906dcc1c2e777d8803973a598a3095f7276b1049498f4b7b2f"
-    sha256 cellar: :any, sonoma:        "9eb0fb3483089f0208c8cdf7cf2b228e8a3c0e6279c65eb2fd62423c284b6e66"
-    sha256 cellar: :any, arm64_linux:   "4b42b304b07733c1f923b042d9d026e83f853dafe9e6f89af360fcabbc812a90"
-    sha256 cellar: :any, x86_64_linux:  "c5ec60d8994ca2d1d01dadae648e52fcc35d61a35f8b184293642f765291cb6b"
+    sha256 cellar: :any, arm64_tahoe:   "e92494c61ad0dbfdab55cb7f69c1e60dbbe164435e512a6a5a2dde8e64f4decb"
+    sha256 cellar: :any, arm64_sequoia: "4834db2346adb29b7eea6216cac56fce7d5cbb5821f4150547cef33e8a33d8a3"
+    sha256 cellar: :any, arm64_sonoma:  "25d37362df0a139de8562e524a66640c73b179a79f3a6edcaeab0e70b44b2cbc"
+    sha256 cellar: :any, arm64_linux:   "e8bdccf963643d8f5b113214da4562c4a6aa5dbc7873340b8a4b87576a4c0159"
+    sha256 cellar: :any, x86_64_linux:  "b80100693ddc253eee6c26df6157795a1fe24e40466caccfea44069b8e3e8c19"
   end
 
   depends_on "rust" => :build
