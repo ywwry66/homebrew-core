0865cd0d17b896a1dbd72e1cfa62f6cae968823c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-29 12:10:23 +0000

    xtermcontrol 3.11

--- Formula/x/xtermcontrol.rb
+++ Formula/x/xtermcontrol.rb
@@ -1,8 +1,8 @@
 class Xtermcontrol < Formula
   desc "Control xterm properties such as colors, title, font and geometry"
   homepage "https://thrysoee.dk/xtermcontrol/"
-  url "https://thrysoee.dk/xtermcontrol/xtermcontrol-3.10.tar.gz"
-  sha256 "3eb97b1d9d8aae1bad4fe2c41ca3a3dbb10d2d67e6ca4599aa1f631a40503dee"
+  url "https://thrysoee.dk/xtermcontrol/xtermcontrol-3.11.tar.gz"
+  sha256 "49ea6d3eda0dbcf875363763cefe1818ce6786b9910255ea641d9786bdafd44c"
   license "GPL-2.0-or-later"
 
   livecheck do
@@ -28,14 +28,7 @@ class Xtermcontrol < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/xtermcontrol --version")
-
-    expected = if OS.mac?
-      "--get-fg is unsupported or disallowed by this terminal"
-    else
-      "failed to get controlling terminal"
-    end
-
-    ret_code = OS.mac? ? 0 : 1
-    assert_match expected, shell_output("#{bin}/xtermcontrol --force --get-fg 2>&1", ret_code)
+    expected = "--get-fg is unsupported or disallowed by this terminal"
+    assert_match expected, shell_output("#{bin}/xtermcontrol --force --get-fg 2>&1", 1)
   end
 end
