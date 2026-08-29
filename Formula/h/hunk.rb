a4bd0958437448de6711e1a33e8f94fb0ba2847c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-29 14:23:46 +0000

    hunk: update 0.20.1 bottle.

--- Formula/h/hunk.rb
+++ Formula/h/hunk.rb
@@ -12,12 +12,11 @@ class Hunk < Formula
   end
 
   bottle do
-    sha256                               arm64_tahoe:   "6e2e3415c2fcea4c2d6f88512fa3a34faa23994753471b63bd9e9a3247062826"
-    sha256                               arm64_sequoia: "6e2e3415c2fcea4c2d6f88512fa3a34faa23994753471b63bd9e9a3247062826"
-    sha256                               arm64_sonoma:  "6e2e3415c2fcea4c2d6f88512fa3a34faa23994753471b63bd9e9a3247062826"
-    sha256 cellar: :any_skip_relocation, sonoma:        "56a296f926977388ad6634ac785442cc6ad90962d7fa0dcd2a4a5fc5184c373b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5f09dd0c3e7b8340123e2a8fa86de39c5204ebeab2734f304746b39c8bdd74f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61c072d22d97302144e08b6763fcbcbcc2fdab5b785e40af59b52e52084a2b05"
+    sha256                               arm64_tahoe:   "e18471769f711a33e006009a2c0cbe680ef55ded798a6e55968e3db78c1a92d8"
+    sha256                               arm64_sequoia: "e18471769f711a33e006009a2c0cbe680ef55ded798a6e55968e3db78c1a92d8"
+    sha256                               arm64_sonoma:  "e18471769f711a33e006009a2c0cbe680ef55ded798a6e55968e3db78c1a92d8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "72f3707988210371916a50799a25e3f73ae3234890a2a2dda339d66ae318b7d6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7b081942696923580a53d63c05b3d6cba4abe4399066e2e5d930ddb2bb72c765"
   end
 
   depends_on "bun" => :build
