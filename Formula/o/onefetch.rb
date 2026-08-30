d9edb6e89d3f608a134505ecac135115f2e28e81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-30 11:41:19 +0000

    onefetch 2.28.1

--- Formula/o/onefetch.rb
+++ Formula/o/onefetch.rb
@@ -1,8 +1,8 @@
 class Onefetch < Formula
   desc "Command-line Git information tool"
   homepage "https://onefetch.dev/"
-  url "https://github.com/o2sh/onefetch/archive/refs/tags/2.27.1.tar.gz"
-  sha256 "3a6f82d3da4da62b2e5406bbe307b0afc73cd8fcc4855534886d80ea0121cc03"
+  url "https://github.com/o2sh/onefetch/archive/refs/tags/2.28.1.tar.gz"
+  sha256 "d51e7411588b3aa8c4d747199941d93b8eb7878d8cfd605463a4c3da125b6be7"
   license "MIT"
   head "https://github.com/o2sh/onefetch.git", branch: "main"
 
