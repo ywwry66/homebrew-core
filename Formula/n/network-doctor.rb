8b6cded453d597acc3ba5506908b0399ce62cc81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 08:50:40 +0000

    network-doctor 1.15.1

--- Formula/n/network-doctor.rb
+++ Formula/n/network-doctor.rb
@@ -1,8 +1,8 @@
 class NetworkDoctor < Formula
   desc "Network troubleshooting TUI"
   homepage "https://github.com/heymaikol/network-doctor/"
-  url "https://github.com/heymaikol/network-doctor/archive/refs/tags/v1.15.0.tar.gz"
-  sha256 "ee5ab98e9eddb26f389ad898e3ce14674d6dde2dd841f95be963ecaecfe8f7e7"
+  url "https://github.com/heymaikol/network-doctor/archive/refs/tags/v1.15.1.tar.gz"
+  sha256 "39d24235a5774783ed969b520f503c538afe0d44a288b7398edbeaf9f69ea4a0"
   license "Apache-2.0"
 
   bottle do
