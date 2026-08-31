45a08924c815c6cae9ce868ac8cf82ec2684c5b8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 20:23:51 +0000

    closure-compiler: update 20260830 bottle.

--- Formula/c/closure-compiler.rb
+++ Formula/c/closure-compiler.rb
@@ -11,7 +11,7 @@ class ClosureCompiler < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, all: "de787c0e94e63854883e16db72fc856b989b996bbe49d3e75009610dfbaf5d02"
+    sha256 cellar: :any_skip_relocation, all: "3ba2b385683516b162a5e21b6c2cf006f32b97a2f4cbceb10a771706b1212d13"
   end
 
   depends_on "openjdk"
