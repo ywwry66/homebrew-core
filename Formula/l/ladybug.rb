b74dbacd16683b419ad20e13d84c43c50ee89ddd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 05:34:24 +0000

    ladybug 0.20.1

--- Formula/l/ladybug.rb
+++ Formula/l/ladybug.rb
@@ -1,8 +1,8 @@
 class Ladybug < Formula
   desc "Embedded graph database built for query speed and scalability"
   homepage "https://ladybugdb.com/"
-  url "https://github.com/LadybugDB/ladybug/archive/refs/tags/v0.20.0.tar.gz"
-  sha256 "19b91c9a291e77ad71b3acb231ecbd052df75d2fcabbf47582cb3d9807ee8119"
+  url "https://github.com/LadybugDB/ladybug/archive/refs/tags/v0.20.1.tar.gz"
+  sha256 "cacec89bd105d8f0da73537054e471e818c983cf9b453e79ca17bd5d7afaefbe"
   license "MIT"
 
   bottle do
@@ -32,26 +32,8 @@ class Ladybug < Formula
     cause "Requires C++20 std::format, https://gcc.gnu.org/gcc-13/changes.html#libstdcxx"
   end
 
-  # Add missing <algorithm> include, upstream PR, https://github.com/LadybugDB/ladybug/pull/855
-  patch do
-    url "https://github.com/LadybugDB/ladybug/commit/1411b812e9d9a40cb0129fda76e72c902fb1f3d8.patch?full_index=1"
-    sha256 "a008f8a9bb4913ed2a743cd02149992f04de90abf6fdd118c14025798dbd9772"
-    type :unofficial
-    resolves "https://github.com/LadybugDB/ladybug/pull/855"
-  end
-
-  # Add more standard library includes that libc++ 23 no longer provides transitively
-  patch do
-    url "https://github.com/LadybugDB/ladybug/commit/464d7f38134dc2b45ad7b0d8ebf8a7943a748f0d.patch?full_index=1"
-    sha256 "76f344776c4a992eb26d22063d9ab72a260a948f8e98c640e3dea8ac2d4e91b2"
-    type :unofficial
-    resolves "https://github.com/LadybugDB/ladybug/pull/860"
-  end
-
   def install
-    args = %W[
-      -DCMAKE_INSTALL_RPATH=#{rpath}
-    ]
+    args = %W[-DCMAKE_INSTALL_RPATH=#{rpath}]
 
     system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
