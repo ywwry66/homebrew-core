9a362ab010eb9fb2f9e351f5df5c4b04890067c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-31 17:05:16 +0000

    gitea-runner 3.3.2

--- Formula/g/gitea-runner.rb
+++ Formula/g/gitea-runner.rb
@@ -1,8 +1,8 @@
 class GiteaRunner < Formula
   desc "Official Actions runner for Gitea"
   homepage "https://gitea.com/gitea/runner"
-  url "https://gitea.com/gitea/runner/archive/v3.3.1.tar.gz"
-  sha256 "a0146974eacddbb167e8316350a4b1fc1fa3010730866ec646ea660c008dfc67"
+  url "https://gitea.com/gitea/runner/archive/v3.3.2.tar.gz"
+  sha256 "8f08d06598e729a6e3e97bc63b470c66ffd6e620c7e154d1bbad4644b30b06f1"
   license "MIT"
 
   bottle do
