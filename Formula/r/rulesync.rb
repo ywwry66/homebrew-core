d41e143cdc5a0a4652b708b79fc3ab5853157f6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 12:27:51 +0000

    rulesync: update 16.19.0 bottle.

--- Formula/r/rulesync.rb
+++ Formula/r/rulesync.rb
@@ -6,11 +6,11 @@ class Rulesync < Formula
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7eb027f5c8eb497a88cb0286d0695e701139cdeed5be57325ab609df6b53a835"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7eb027f5c8eb497a88cb0286d0695e701139cdeed5be57325ab609df6b53a835"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7eb027f5c8eb497a88cb0286d0695e701139cdeed5be57325ab609df6b53a835"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "301e43a912aeeabd4d84b6a05eadf5ffac5860d73aec088dfe9fc47ccd07806a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "301e43a912aeeabd4d84b6a05eadf5ffac5860d73aec088dfe9fc47ccd07806a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7fb7d7b03fef2929ad6487e959a5395c0159dd46ff755c095733a3ba77337079"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7fb7d7b03fef2929ad6487e959a5395c0159dd46ff755c095733a3ba77337079"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7fb7d7b03fef2929ad6487e959a5395c0159dd46ff755c095733a3ba77337079"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "72568cee1aab276ec0223b97fb980ab1943b74d0fd119a061a0b623155070d07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "72568cee1aab276ec0223b97fb980ab1943b74d0fd119a061a0b623155070d07"
   end
 
   depends_on "node"
