ab8f4678ca08f461804dfa639f078bf2023d8bf3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 09:24:17 +0000

    vespa-cli: update 8.748.3 bottle.

--- Formula/v/vespa-cli.rb
+++ Formula/v/vespa-cli.rb
@@ -12,12 +12,11 @@ class VespaCli < Formula
   end
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "309d1b996a44ba4071147f7333ca562c32bbbfd27345e9639390c528211612d8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "01482b0f13582a798c81d9c2128b17d6922df077249d95c1d9b4d4f107579a5c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "27cf6f4596917c15cd6ae21083f30bde97b377d58c979dfc3c074e530ab00c54"
-    sha256 cellar: :any_skip_relocation, sonoma:        "d5939d8060cf290dd1ecc45db7c1417017c4935e832d194f0955dd70b9ad0e7b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "28250cc786c315c1b7c28236440a242ae478d650802c2e9b8cf6f1d67b894924"
-    sha256 cellar: :any,                 x86_64_linux:  "cb9f9d39ca3744c4830039d507e6621af8d5d77b924505d7761149ac9a80f64f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "79c5170999cb0c0a3a7d73cc32e6c4993e4ea9ead5d297dc22dd8f1d021fc126"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c7bc1bba9f79866bb5a1f6fed1cf22048f78a2ab79b668c0d47b13d69f45147d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5927c919634176af3187d1446481438d41f69a7cf668835cc1e1167ed87bd80e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "81b245969b0a89cc7ecabc132d06b7da67e1116f3f1895c93cd036d235464a0b"
+    sha256 cellar: :any,                 x86_64_linux:  "1363da024a75e7f2c6572e9d1135f5c0a3cc51868de71588e5cb90ebc86426c5"
   end
 
   depends_on "go" => :build
