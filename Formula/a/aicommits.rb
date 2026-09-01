074a6548faf3c1605439194d0050569f3380d62d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 13:49:59 +0000

    aicommits 4.2.0

--- Formula/a/aicommits.rb
+++ Formula/a/aicommits.rb
@@ -1,8 +1,8 @@
 class Aicommits < Formula
   desc "Writes your git commit messages for you with AI"
   homepage "https://github.com/Nutlope/aicommits"
-  url "https://registry.npmjs.org/aicommits/-/aicommits-4.1.1.tgz"
-  sha256 "a76055878d2c3b89e669b59c8cd2b81316c4400952508d97238f66304fa10aec"
+  url "https://registry.npmjs.org/aicommits/-/aicommits-4.2.0.tgz"
+  sha256 "2f0b38e8601c6c39ba408cecddc52256f5d7ce5e1a5be8fa49ac1e2f08e3df8d"
   license "MIT"
 
   bottle do
