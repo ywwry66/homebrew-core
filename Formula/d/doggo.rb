a98c521abfacfeafeed73dc2b92c6518881a6712
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-01 11:44:45 +0000

    doggo: update 1.4.0 bottle.

--- Formula/d/doggo.rb
+++ Formula/d/doggo.rb
@@ -7,12 +7,11 @@ class Doggo < Formula
   head "https://github.com/mr-karan/doggo.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b85aaee2e993de9cd31f5ff8d4f1d779529904f6415f39b0e948964a1756e216"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b85aaee2e993de9cd31f5ff8d4f1d779529904f6415f39b0e948964a1756e216"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b85aaee2e993de9cd31f5ff8d4f1d779529904f6415f39b0e948964a1756e216"
-    sha256 cellar: :any_skip_relocation, sonoma:        "1af29050c243ef3b023af14043e534f384cfacc7583df4376d3d07f2d703d696"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5042c3e3fbd93176240f94b648a4e7819cb1f6058c949eb87e2439856e0e04d4"
-    sha256 cellar: :any,                 x86_64_linux:  "ef262c3e984ff4c920f09946ebef0934d455cb4219028d7bce2dc16ddc195ec7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e805f38eb20b5ca3fe542b6514c41d0565116973718b592bf8f07673707fc130"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e805f38eb20b5ca3fe542b6514c41d0565116973718b592bf8f07673707fc130"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e805f38eb20b5ca3fe542b6514c41d0565116973718b592bf8f07673707fc130"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "634f72e44332cf18023aeaccabf6b104eb3166c8c71fce16557a1ca5cca9e74e"
+    sha256 cellar: :any,                 x86_64_linux:  "174fd0f70d500a3aa1788391c3773beef6fac920ec8f4b878247d1723436c33c"
   end
 
   depends_on "go" => :build
